039332cb9cca24a8d07f3d192db0db652e012ce3
Carter Tran <carter.tran@gmail.com>
2024-10-17 17:12:35 -0700

    update data flow for dropdown and df

--- app.py
+++ app.py
@@ -1,76 +1,109 @@
-import plotly.express as px
-import plotly.graph_objects as go
-
-import pandas as pd
 import dask.dataframe as dd
+import plotly.express as px
 
-from dash import Dash, html, dcc, Input, Output, State, callback, dash_table
+from dash_extensions.enrich import (
+    DashProxy,
+    html,
+    dcc,
+    Input,
+    Output,
+    State,
+    Serverside,
+    ServersideOutputTransform,
+)
+
 import io
 import base64
+import logging
 
 from utils import parse_xml, ts_type
 
-import logging
 
-app = Dash(__name__)
-
-df = dd.read_parquet('data/carter_export.parquet')
+app = DashProxy(__name__, transforms=[ServersideOutputTransform()])
 
 server = app.server
 
-valid_types = [_type for _type in df['type'].unique().compute() if 'nan' not in _type]
+
+def valid_types(_df=dd.read_parquet("data/carter_export.parquet")):
+    return [_type for _type in _df["type"].unique().compute() if "nan" not in _type]
+
 
 app.layout = [
-    html.H1("Team 204 - Automated Health Insights from Wearable Devices", style={'textAlign': 'center'}),
-    html.P("By default we display metrics based on Carter's Apple Watch data. Note that he switched to the Apple Watch Ultra in April 2023, so metrics may be measured slightly differently before and after this date.You can upload your own data to visualize your own metrics."),
+    html.H1(
+        "Team 204 - Automated Health Insights from Wearable Devices",
+        style={"textAlign": "center"},
+    ),
+    html.P(
+        "By default we display metrics based on Carter's Apple Watch data. Note that he switched to the Apple Watch Ultra in April 2023, so metrics may be measured slightly differently before and after this date.You can upload your own data to visualize your own metrics."
+    ),
     dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            html.A('Upload Export.xml')
-        ]),
-        accept='.xml',
+        id="upload-data",
+        children=html.Div([html.A("Upload Export.xml")]),
+        accept=".xml",
         style={
-            'width': '240px',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': 'auto'
+            "width": "240px",
+            "height": "60px",
+            "lineHeight": "60px",
+            "borderWidth": "1px",
+            "borderStyle": "dashed",
+            "borderRadius": "5px",
+            "textAlign": "center",
+            "margin": "auto",
         },
     ),
     html.Br(),
     html.P("Select the metrics you want to visualize:"),
     dcc.Dropdown(
-        id='type-dropdown',
-        options=[{'label': _type, 'value': _type} for _type in valid_types],
-        value=['Distance Walking Running (mi)'],
+        id="type-dropdown",
+        options=valid_types(),
+        value=[],
         multi=True,
     ),
-    html.Div(id='output-data-upload'),
+    html.Div(id="metric-graphs"),
+    dcc.Loading(dcc.Store(id="df-store"), fullscreen=True, type="default"),
 ]
 
-@callback(
-    Output('output-data-upload', 'children'),
-    Input('type-dropdown', 'value'),
-    Input('upload-data', 'contents'),
-    State('upload-data', 'filename'),
+
+@app.callback(
+    Output("type-dropdown", "options"),
+    Input("df-store", "data"),
+    prevent_initial_call=True,
 )
-def update_output(types, contents, filename):
-    global df
+def valid_types_callback(_df):
+    return valid_types(_df)
+
+
+@app.callback(
+    Output("df-store", "data"),
+    Input("upload-data", "contents"),
+    State("upload-data", "filename"),
+)
+def update_df(contents, filename):
     if contents:
-        content_type, content_string = contents.split(',')
+        content_type, content_string = contents.split(",")
         decoded = base64.b64decode(content_string)
 
         try:
             logging.info(f"Parsing XML file: {filename}")
-            df = parse_xml(io.StringIO(decoded.decode('utf-8')))
+            _df = parse_xml(io.StringIO(decoded.decode("utf-8")))
         except Exception as e:
             logging.error(e)
+    else:
+        _df = dd.read_parquet("data/carter_export.parquet")
+    logging.debug(f"Updated df:\n{_df.head()}")
+    return Serverside(_df)
+
+
+@app.callback(
+    Output("metric-graphs", "children"),
+    Input("df-store", "data"),
+    Input("type-dropdown", "value"),
+    prevent_initial_call=True,
+)
+def update_metric_graphs(_df, types):
     figs = []
     for col in types:
-        series = ts_type(df, col)
+        series = ts_type(_df, col)
         fig = px.line(series, title=series.name)
         fig.update_layout(
             xaxis_title="Date",
@@ -81,6 +114,6 @@ def update_output(types, contents, filename):
     return [dcc.Graph(figure=fig) for fig in figs]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,87 +1,105 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 import dask.dataframe as dd
-import datetime as dt
 
 import logging
 import time
 from pathlib import Path
 
+
 def clean_type(df, _type):
     name = str(_type)
     try:
-        unit = df[df['type'] == name]['unit'].astype(str).dropna().compute().iloc[0]
+        unit = df[df["type"] == name]["unit"].astype(str).dropna().compute().iloc[0]
         logging.info(f"Detected unit: {unit} for type: {_type}")
     except Exception as e:
         logging.info(f"Cannot detect unit for type: {_type}")
         logging.debug(e)
-        unit = 'NO_UNIT'
-    name = name.replace('HKQuantityTypeIdentifier', '').replace('HKCategoryTypeIdentifier', '')
+        unit = "NO_UNIT"
+    name = name.replace("HKQuantityTypeIdentifier", "").replace(
+        "HKCategoryTypeIdentifier", ""
+    )
     i = 1
     while i < len(name):
-        if name[i].isupper() and name[i-1].islower():
-            name = name[:i] + ' ' + name[i:]
+        if name[i].isupper() and name[i - 1].islower():
+            name = name[:i] + " " + name[i:]
             i += 1
         i += 1
-    name += f' ({unit})'
+    name += f" ({unit})"
     return name.strip()
 
+
 def clean_types(df):
-    mapping = {
-        _type: clean_type(df, _type) for _type in df['type'].unique()
-    }
-    return df.assign(type=df['type'].cat.rename_categories(mapping))
+    mapping = {_type: clean_type(df, _type) for _type in df["type"].unique()}
+    return df.assign(type=df["type"].cat.rename_categories(mapping))
+
 
 def parse_xml(filepath):
-
     logging.debug("Parsing XML Element Tree")
     tree = ET.parse(filepath)
 
     logging.debug("Getting root")
     root = tree.getroot()
-    record_list = [x.attrib for x in root.iter('Record')]
+    record_list = [x.attrib for x in root.iter("Record")]
 
     logging.debug("Creating Dask DataFrame")
     df = dd.from_pandas(pd.DataFrame(record_list), npartitions=10)
 
     logging.debug("Assigning dates and converting to numeric")
     df = df.assign(
-        creationDate=lambda x: dd.to_datetime(x['creationDate']),
-        startDate=lambda x: dd.to_datetime(x['startDate']),
-        endDate=lambda x: dd.to_datetime(x['endDate']),
-        value=lambda x: dd.to_numeric(x['value'], errors='coerce')
+        creationDate=lambda x: dd.to_datetime(x["creationDate"]),
+        startDate=lambda x: dd.to_datetime(x["startDate"]),
+        endDate=lambda x: dd.to_datetime(x["endDate"]),
+        value=lambda x: dd.to_numeric(x["value"], errors="coerce"),
     )
 
-    dtype={'type': 'category', 'sourceName': 'category', 'sourceVersion': 'category', 'unit': 'category', 'value': 'float32', 'device': 'str'}
+    dtype = {
+        "type": "category",
+        "sourceName": "category",
+        "sourceVersion": "category",
+        "unit": "category",
+        "value": "float32",
+        "device": "str",
+    }
 
     return df.astype(dtype)
 
 
 def parse_csv(filepath):
-    df = dd.read_csv(filepath, dtype={'type': 'category', 'sourceName': 'category', 'sourceVersion': 'category', 'unit': 'category', 'value': 'float32', 'device': 'str'})
+    df = dd.read_csv(
+        filepath,
+        dtype={
+            "type": "category",
+            "sourceName": "category",
+            "sourceVersion": "category",
+            "unit": "category",
+            "value": "float32",
+            "device": "str",
+        },
+    )
     df = df.assign(
-        creationDate=lambda x: dd.to_datetime(x['creationDate']),
-        startDate=lambda x: dd.to_datetime(x['startDate']),
-        endDate=lambda x: dd.to_datetime(x['endDate']),
+        creationDate=lambda x: dd.to_datetime(x["creationDate"]),
+        startDate=lambda x: dd.to_datetime(x["startDate"]),
+        endDate=lambda x: dd.to_datetime(x["endDate"]),
     )
     return df
 
 
-def ts_type(df, _type, resample='1D', resample_agg='sum'):
+def ts_type(df, _type, resample="1D", resample_agg="sum"):
     try:
-        _df = df[df['type'] == _type].set_index('startDate')['value'].rename(_type)
+        _df = df[df["type"] == _type].set_index("startDate")["value"].rename(_type)
         return _df.resample(resample).agg(resample_agg).compute()
     except Exception as e:
         logging.debug(e)
         logging.info(f"{_type} not found in dataframe")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
-    data_dir = Path('data')
-    filename = 'ani_export.xml'
-    filestem = filename.split('.')[0]
+    data_dir = Path("data")
+    filename = "ani_export.xml"
+    filestem = filename.split(".")[0]
     filepath = data_dir / filename
 
     logging.info("Parsing XML file")
@@ -104,12 +122,12 @@ if __name__ == '__main__':
 
     logging.info("Writing to parquet")
     start_time = time.time()
-    df.to_parquet(data_dir / f'{filestem}.parquet')
+    df.to_parquet(data_dir / f"{filestem}.parquet")
     end_time = time.time()
     logging.info(f"Time taken to write to parquet: {end_time - start_time} seconds")
 
     logging.info("Reading from parquet")
     start_time = time.time()
-    df = dd.read_parquet(data_dir / f'{filestem}.parquet')
+    df = dd.read_parquet(data_dir / f"{filestem}.parquet")
     end_time = time.time()
     logging.info(f"Time taken to read from parquet: {end_time - start_time} seconds")
